1b7651ea85b9b5992be30dd214d09f9075bb6fff
Brendan <2bndy5@gmail.com>
2020-03-26 06:13:43 -0700

    decided to not inherit RF24; now a class wrapper

--- circuitpython_nrf24l01/fake_ble.py
+++ circuitpython_nrf24l01/fake_ble.py
@@ -49,8 +49,6 @@ work on CircuitPython.
        other event, "on data fail", is ignored because it will never get thrown with "auto_ack"
        off. However the interrupt settings can be modified AFTER instantiation
 """
-import time
-from .rf24 import RF24
 
 def _swap_bits(orig):
     """reverses the bit order into LSbit to MSBit"""
@@ -107,63 +105,61 @@ def _ble_whitening(data, whiten_coef):
         result += bytes([byte])
     return result
 
-class FakeBLE(RF24):
+class FakeBLE:
     """Per the limitations of this technique, only power amplifier level is available for
     configuration when advertising BLE data.
 
-    :param ~busio.SPI spi: The object for the SPI bus that the nRF24L01 is connected to.
-
-        .. tip:: This object is meant to be shared amongst other driver classes (like
-            adafruit_mcp3xxx.mcp3008 for example) that use the same SPI bus. Otherwise, multiple
-            devices on the same SPI bus with different spi objects may produce errors or
-            undesirable behavior.
-    :param ~digitalio.DigitalInOut csn: The digital output pin that is connected to the
-        nRF24L01's CSN (Chip Select Not) pin. This is required.
-    :param ~digitalio.DigitalInOut ce: The digital output pin that is connected to the
-        nRF24L01's CE (Chip Enable) pin. This is required.
-    :param bytearray name: This will be the nRF24L01-emulated BLE device's broadcasted name.
-        This is option and defaults to `None` to allow for larger paylaods because the name's
-        byte length borrows from the same buffer space that the payload data occupies. See
-        `name` attribute for more details.
-    :param int pa_level: This parameter controls the RF power amplifier setting of transmissions.
-        Options are ``0`` (dBm), ``-6`` (dBm), ``-12`` (dBm), or ``-18`` (dBm). This can be
-        changed at any time by using the `pa_level` attribute.
+    :param ~circuitpython_nrf24l01 nrf: The object for the nRF24L01 transceiver to use
+        for fake BLE advertisements.
+    :param bytearray name: The BLE device name to be advertised with the payload.
     """
-    def __init__(self, spi, csn, ce, name=None, pa_level=False, irq_dr=False, irq_ds=True):
-        super(FakeBLE, self).__init__(spi, csn, ce,
-                                      pa_level=pa_level,
-                                      crc=0,
-                                      dynamic_payloads=False,
-                                      arc=0,
-                                      address_length=4,
-                                      ask_no_ack=False,
-                                      irq_df=False,
-                                      irq_dr=irq_dr,
-                                      irq_ds=irq_ds)
+    def __init__(self, nrf, name=None):
+        self._device = nrf
+        self._device.address_length = 4
+        self._device.dynamic_payloads = False
+        self._device.auto_ack = False
+        self._device.crc = 0
+        self._device.arc = 0
+        self._device.irq_df = False
+        self._device.irq_dr = False
+        self._device.irq_ds = True
         self._chan = 0
         self._ble_name = None
         self.name = name
+        self._device.open_tx_pipe(_reverse_bits(b'\x8E\x89\xBE\xD6'))
+        # b'\x8E\x89\xBE\xD6' = proper address for BLE advertisments
+
+    def __enter__(self):
+        return self._device.__enter__()
+
+    def __exit(self, *exc):
+        return self._device.__exit__(exc)
 
     @property
     def name(self):
-        """Represents the emulated BLE device name during braodcasts. must be a buffer protocol
-        object (`bytearray`) , and can be any length (less than 14) of UTF-8 freindly characters.
+        """Represents the emulated BLE device name during braodcasts. This must
+        be a buffer protocol object (`bytearray`) , and can be any length (less
+        than 14) of UTF-8 freindly characters. Set this to `None` to disable
+        advertising a BLE device name.
 
-        .. note:: the BLE device's name will occupy the same space as your TX data. While space
-            is limited to 32 bytes on the nRF24L01, actual usable BLE TX data = 16 - (name length
-            + 2). The other 16 bytes available on the nRF24L01 TX FIFO buffer are reserved for
-            the [arbitrary] MAC address and other BLE related stuff.
+        .. note:: the BLE device's name will occupy the same space as your TX
+            data. While space is limited to 32 bytes on the nRF24L01, actual
+            usable BLE TX data = 16 - (name length + 2). The other 16 bytes
+            available on the nRF24L01 TX FIFO buffer are reserved for the
+            [arbitrary] MAC address and other BLE related stuff.
         """
         return self._ble_name[2:] if self._ble_name is not None else None
 
     @name.setter
     def name(self, n):
-        """The broadcasted BLE name of the nRF24L01. This is not required. In fact, setting this
-        attribute will subtract from the available payload length (in bytes).
+        """The broadcasted BLE name of the nRF24L01. This is not required. In
+        fact setting this attribute will subtract from the available payload
+        length (in bytes).
 
-            * payload_length has maximum of 19 bytes when NOT broadcasting a name for itself.
-            * payload_length has a maximum of (17 - length of name) bytes when broadcasting a
+            * payload_length has a maximum of 19 bytes when NOT broadcasting a
               name for itself.
+            * payload_length has a maximum of (17 - length of name) bytes when
+              broadcasting a name for itself.
         """
         if n is not None and 1 <= len(n) <= 12: # max defined by 1 byte payload data requisite
             self._ble_name = bytes([len(n) + 1]) + b'\x08' + n
@@ -171,42 +167,38 @@ class FakeBLE(RF24):
             self._ble_name = None # name will not be advertised
 
     def _chan_hop(self):
-        """BLE protocol specs mandate the BLE device cyle through the following 3 channels:
-
-            - nRF channel 2  == BLE channel 37
-            - nRF channel 26 == BLE channel 38
-            - nRF channel 80 == BLE channel 39
-
-        .. note:: then BLE channel number is different from the nRF channel number.
-        """
+        # NOTE BLE channel number is different from the nRF channel number.
+        #     - nRF channel 2  == BLE channel 37
+        #     - nRF channel 26 == BLE channel 38
+        #     - nRF channel 80 == BLE channel 39
         self._chan = (self._chan + 1) if (self._chan + 1) < 3 else 0
-        self.channel = 26 if self._chan == 1 else (80 if self._chan == 2 else 2)
+        self._device.channel = 26 if self._chan == 1 else (80 if self._chan == 2 else 2)
 
-    # pylint: disable=arguments-differ
-    def send(self, buf):
-        """This blocking function is used to transmit payload.
+    def advertise(self, buf):
+        """This blocking function is used to transmit a payload.
 
         :returns: Nothing as every transmission will register as a success under these required
             settings.
 
-        :param bytearray buf: The payload to transmit. This bytearray must have a length greater
-            than 0 and less than 20, otherwise a `ValueError` exception is thrown. This can also
-            be a list or tuple of payloads (`bytearray`); in which case, all items in the
-            list/tuple are processed for consecutive transmissions.
+        :param bytearray buf: The payload to transmit. This bytearray must have
+            a length greater than 0 and less than 20, otherwise a `ValueError`
+            exception is thrown. This can also be a list or tuple of payloads
+            (`bytearray`); in which case, all items in the list/tuple are
+            processed for consecutive transmissions.
 
-            - If the `dynamic_payloads` attribute is disabled and this bytearray's length is less
-              than the `payload_length` attribute, then this bytearray is padded with zeros until
-              its length is equal to the `payload_length` attribute.
-            - If the `dynamic_payloads` attribute is disabled and this bytearray's length is
-              greater than `payload_length` attribute, then this bytearray's length is truncated
-              to equal the `payload_length` attribute.
+            - If the `dynamic_payloads` attribute is disabled and this
+              bytearray's length is less than the `payload_length` attribute,
+              then this bytearray is padded with zeros until its length is
+              equal to the `payload_length` attribute.
+            - If the `dynamic_payloads` attribute is disabled and this
+              bytearray's length is greater than `payload_length` attribute,
+              then this bytearray's length is truncated to equal the
+              `payload_length` attribute.
 
-        .. note:: If the name of the emulated BLE device is also to be braodcast, then the 'name'
-            attribute should be set prior to calling `send()`.
+        .. note:: If the name of the emulated BLE device is also to be
+            broadcast, then the 'name' attribute should be set prior to calling
+            `advertise()`.
         """
-        self.ce_pin.value = 0
-        self.flush_tx()
-        self.clear_status_flags(False) # clears TX related flags only
         # max payload_length = 32 - 14(header, MAC, & CRC) - 2(container header) - 3(BLE flags)
         #                    = 13 - (BLE name length + 2 if any)
         name_len = len(self._ble_name) if self._ble_name is not None else 0
@@ -214,19 +206,17 @@ class FakeBLE(RF24):
             raise ValueError("buf must be a buffer protocol object with a byte length of"
                              " at least 1 and no greater than 13 - "
                              "{} = {}".format(name_len, 13 - name_len))
-
-        # BLE payload = header(1) + payload length(1) + MAC address(6) + containers + CRC(3) bytes
+        # BLE payload =
+        #   header(1) + payload length(1) + MAC address(6) + containers + CRC(3) bytes
         # header == PDU type, given MAC address is random/arbitrary
         #               type == 0x42 for Android or 0x40 for iPhone
         # containers (in bytes) = length(1) + type(1) + data
         # the 1 byte about container's length excludes only itself
-
         payload = b'\x42' # init a temp payload buffer with header type byte
-
         # to avoid padding when dynamic_payloads is disabled, set payload_length attribute
-        self.payload_length = len(buf) + 16 + name_len
+        self._device.payload_length = len(buf) + 16 + name_len
         # payload length excludes the header, itself, and crc lengths
-        payload += bytes([self.payload_length - 5])
+        payload += bytes([self._device.payload_length - 5])
         payload += b'\x11\x22\x33\x44\x55\x66' # a bogus MAC address
         # payload will have at least 2 containers:
         # 3 bytes of flags (required for BLE discoverable), & at least (1+2) byte of data
@@ -242,60 +232,6 @@ class FakeBLE(RF24):
         # the whiten_coef value we need is the BLE channel (37,38, or 39) left shifted one
         whiten_coef = 37 + self._chan
         whiten_coef = _swap_bits(whiten_coef) | 2
-
-        print('transmitting {} as {}'.format(payload,
-                                             _reverse_bits(_ble_whitening(payload, whiten_coef))))
-        # init using non-blocking helper
-        self.write(_reverse_bits(_ble_whitening(payload, whiten_coef)))
-        time.sleep(0.00001) # ensure CE pulse is >= 10 µs
-        # pulse is stopped here; the nRF24L01 only handles the top level payload in the FIFO.
-        self.ce_pin.value = 0 # go to Standby-I power mode (power attribute still == True)
-
-        # T_upload is done before timeout begins (after payload write action AKA upload)
-        timeout = (((8 * (5 + len(payload))) + 9) / 125000) + 0.0002682
-        start = time.monotonic()
-        while not self.irq_ds and (time.monotonic() - start) < timeout:
-            self.update() # perform Non-operation command to get status byte (should be faster)
-            # print('status: DR={} DS={} DF={}'.format(self.irq_dr, self.irq_ds, self.irq_df))
-        self.clear_status_flags(False) # only TX related IRQ flags
-
-    # Altering all the following settings is disabled
-    def open_tx_pipe(self):
-        super(FakeBLE, self).open_tx_pipe(_reverse_bits(b'\x8E\x89\xBE\xD6'))
-        # b'\x8E\x89\xBE\xD6' = proper address for BLE advertisments
-    # pylint: enable=arguments-differ
-
-    # ignore pylint errors when overriding setter functions of inherited attributes
-    # pylint: disable=no-member
-    @RF24.address_length.setter
-    def address_length(self, t):
-        super(FakeBLE, self).address_length = (4 + t * 0)
-
-    @RF24.listen.setter
-    def listen(self, rx):
-        if self.listen or rx:
-            self._stop_listening()
-
-    @RF24.data_rate.setter
-    def data_rate(self, t):
-        super(FakeBLE, self).data_rate = (1 + t * 0)
-
-    @RF24.dynamic_payloads.setter
-    def dynamic_payloads(self, t):
-        super(FakeBLE, self).dynamic_payloads = (False & t)
-
-    @RF24.auto_ack.setter
-    def auto_ack(self, t):
-        super(FakeBLE, self).auto_ack = (False & t)
-
-    @RF24.ack.setter
-    def ack(self, t):
-        super(FakeBLE, self).ack = (False & t)
-
-    @RF24.crc.setter
-    def crc(self, t):
-        super(FakeBLE, self).crc = (0 * t)
-
-    @RF24.arc.setter
-    def arc(self, t):
-        super(FakeBLE, self).arc = (t * 0)
+        rev_whiten_pl = _reverse_bits(_ble_whitening(payload, whiten_coef))
+        print('transmitting {} as {}'.format(payload, rev_whiten_pl))
+        self._device.send(rev_whiten_pl)
