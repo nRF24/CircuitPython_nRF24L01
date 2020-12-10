9577188e6b4c9aa071520760f7b5d3597761e6c3
brendan <2bndy5@gmail.com>
2020-12-10 02:29:00 -0800

    missed renaming a couple FakeBLE.available() calls

--- circuitpython_nrf24l01/fake_ble.py
+++ circuitpython_nrf24l01/fake_ble.py
@@ -174,10 +174,10 @@ class FakeBLE:
 
     def _make_payload(self, payload):
         """Assemble the entire packet to be transmitted as a payload."""
-        if self.available(payload) < 0:
+        if self.len_available(payload) < 0:
             raise ValueError(
                 "Payload length exceeds maximum buffer size by "
-                "{} bytes".format(abs(self.available(payload)))
+                "{} bytes".format(abs(self.len_available(payload)))
             )
         name_length = (len(self.name) + 2) if self.name is not None else 0
         pl_size = 9 + len(payload) + name_length + self._show_dbm * 3
