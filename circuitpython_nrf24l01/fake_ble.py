f83060d8ffe5f103e61b41644cf6fc38a6bc12c0
Brendan <2bndy5@gmail.com>
2021-06-18 02:57:06 -0700

    undo type hinting in fake_ble.py

--- circuitpython_nrf24l01/fake_ble.py
+++ circuitpython_nrf24l01/fake_ble.py
@@ -24,11 +24,10 @@ found here
 <http://dmitry.gr/index.php?r=05.Projects&proj=11.%20Bluetooth%20LE%20fakery>`_"""
 from os import urandom
 import struct
-from typing import Union
-from .rf24 import RF24, SPIDevice, DigitalInOut
+from .rf24 import RF24
 
 
-def swap_bits(original: int) -> int:
+def swap_bits(original):
     """This function reverses the bit order for a single byte."""
     original &= 0xFF
     reverse = 0
@@ -39,7 +38,7 @@ def swap_bits(original: int) -> int:
     return reverse
 
 
-def reverse_bits(original: Union[bytearray, bytes]) -> bytearray:
+def reverse_bits(original):
     """This function reverses the bit order for an entire
     buffer protocol object."""
     ret = bytearray(len(original))
@@ -48,13 +47,13 @@ def reverse_bits(original: Union[bytearray, bytes]) -> bytearray:
     return ret
 
 
-def chunk(buf: Union[bytearray, bytes], data_type: int=0x16) -> bytearray:
+def chunk(buf, data_type=0x16):
     """This function is used to pack data values into a block of data that
     make up part of the BLE payload per Bluetooth Core Specifications."""
     return bytearray([len(buf) + 1, data_type & 0xFF]) + buf
 
 
-def crc24_ble(data, deg_poly: int=0x65B, init_val: int=0x555555) -> bytearray:
+def crc24_ble(data, deg_poly=0x65B, init_val=0x555555):
     """This function calculates a checksum of various sized buffers."""
     crc = init_val
     for byte in data:
@@ -75,12 +74,7 @@ BLE_FREQ = (2, 26, 80)
 class FakeBLE(RF24):
     """A class to implement BLE advertisements using the nRF24L01."""
 
-    def __init__(self,
-            spi: SPIDevice,
-            csn: DigitalInOut,
-            ce_pin: DigitalInOut,
-            spi_frequency: int=10000000
-        ) -> None:
+    def __init__(self, spi, csn, ce_pin, spi_frequency=10000000):
         super().__init__(spi, csn, ce_pin, spi_frequency=spi_frequency)
         self._curr_freq = 0
         self._show_dbm = False
@@ -100,13 +94,13 @@ class FakeBLE(RF24):
         return super().__exit__()
 
     @property
-    def mac(self) -> bytearray:
+    def mac(self):
         """This attribute returns a 6-byte buffer that is used as the
         arbitrary mac address of the BLE device being emulated."""
         return self._mac
 
     @mac.setter
-    def mac(self, address: Union[bytes, bytearray]) -> None:
+    def mac(self, address):
         if address is None:
             self._mac = urandom(6)
         if isinstance(address, int):
@@ -117,37 +111,37 @@ class FakeBLE(RF24):
             self._mac += urandom(6 - len(self._mac))
 
     @property
-    def name(self) -> bytearray:
+    def name(self):
         """The broadcasted BLE name of the nRF24L01."""
         return self._ble_name
 
     @name.setter
-    def name(self, n: Union[bytes, bytearray]) -> None:
-        if n is not None:
-            if not isinstance(n, (bytes, bytearray)):
+    def name(self, _name):
+        if _name is not None:
+            if not isinstance(_name, (bytes, bytearray)):
                 raise ValueError("name must be a bytearray or bytes object.")
-            if len(n) > (18 - self._show_dbm * 3):
+            if len(_name) > (18 - self._show_dbm * 3):
                 raise ValueError("name length exceeds maximum.")
-        self._ble_name = n
+        self._ble_name = _name
 
     @property
-    def show_pa_level(self) -> bool:
+    def show_pa_level(self):
         """If this attribute is `True`, the payload will automatically include
         the nRF24L01's `pa_level` in the advertisement."""
         return bool(self._show_dbm)
 
     @show_pa_level.setter
-    def show_pa_level(self, enable: bool) -> None:
+    def show_pa_level(self, enable):
         if enable and len(self.name) > 16:
             raise ValueError("there is not enough room to show the pa_level.")
         self._show_dbm = bool(enable)
 
-    def hop_channel(self) -> None:
+    def hop_channel(self):
         """Trigger an automatic change of BLE compliant channels."""
         self._curr_freq += 1 if self._curr_freq < 2 else -2
         self.channel = BLE_FREQ[self._curr_freq]
 
-    def whiten(self, data: Union[bytes, bytearray]) -> bytearray:
+    def whiten(self, data):
         """Whitening the BLE packet data ensures there's no long repetition
         of bits."""
         data, coef = (bytearray(data), (self._curr_freq + 37) | 0x40)
@@ -167,7 +161,7 @@ class FakeBLE(RF24):
         # print("whitened: 0x{}".format(address_repr(data)))
         return data
 
-    def _make_payload(self, payload: Union[bytes, bytearray]) -> bytearray:
+    def _make_payload(self, payload):
         """Assemble the entire packet to be transmitted as a payload."""
         if self.len_available(payload) < 0:
             raise ValueError(
@@ -191,13 +185,13 @@ class FakeBLE(RF24):
         buf += crc24_ble(buf)
         return buf
 
-    def len_available(self, hypothetical: Union[bytes, bytearray]=b"") -> int:
+    def len_available(self, hypothetical=b""):
         """This function will calculates how much length (in bytes) is
         available in the next payload."""
         name_length = (len(self.name) + 2) if self.name is not None else 0
         return 18 - name_length - self._show_dbm * 3 - len(hypothetical)
 
-    def advertise(self, buf: Union[bytes, bytearray]=b"", data_type: int=0xFF) -> None:
+    def advertise(self, buf=b"", data_type=0xFF):
         """This blocking function is used to broadcast a payload."""
         if not isinstance(buf, (bytearray, bytes, list, tuple)):
             raise ValueError("buffer is an invalid format")
@@ -212,13 +206,13 @@ class FakeBLE(RF24):
         # print("reversed: 0x{}".format(address_repr(reverse_bits(payload))))
         self.send(reverse_bits(payload))
 
-    def print_details(self, dump_pipes: bool=False) -> None:
+    def print_details(self, dump_pipes=False):
         super().print_details(dump_pipes)
         print("BLE device name___________{}".format(str(self.name)))
         print("Broadcasting PA Level_____{}".format(self.show_pa_level))
 
     @RF24.channel.setter
-    def channel(self, value: int) -> None:
+    def channel(self, value):
         if value in BLE_FREQ:
             self._channel = value
             self._reg_write(0x05, value)
@@ -227,33 +221,33 @@ class FakeBLE(RF24):
 
     # pylint: disable=unused-argument
     @RF24.dynamic_payloads.setter
-    def dynamic_payloads(self, val) -> None:
+    def dynamic_payloads(self, val):
         raise RuntimeWarning("adjusting dynamic_payloads breaks BLE specifications")
 
     @RF24.data_rate.setter
-    def data_rate(self, val) -> None:
+    def data_rate(self, val):
         raise RuntimeWarning("adjusting data_rate breaks BLE specifications")
 
     @RF24.address_length.setter
-    def address_length(self, val) -> None:
+    def address_length(self, val):
         raise RuntimeWarning("adjusting address_length breaks BLE specifications")
 
     @RF24.auto_ack.setter
-    def auto_ack(self, val) -> None:
+    def auto_ack(self, val):
         raise RuntimeWarning("adjusting auto_ack breaks BLE specifications")
 
     @RF24.ack.setter
-    def ack(self, val) -> None:
+    def ack(self, val):
         raise RuntimeWarning("adjusting ack breaks BLE specifications")
 
     @RF24.crc.setter
-    def crc(self, val) -> None:
+    def crc(self, val):
         raise RuntimeWarning("adjusting crc breaks BLE specifications")
 
-    def open_rx_pipe(self, pipe_number, address) -> None:
+    def open_rx_pipe(self, pipe_number, address):
         raise RuntimeWarning("BLE implementation only uses 1 address on pipe 0")
 
-    def open_tx_pipe(self, address) -> None:
+    def open_tx_pipe(self, address):
         raise RuntimeWarning("BLE implentation only uses 1 address")
 
     # pylint: enable=unused-argument
@@ -263,34 +257,34 @@ class ServiceData:
     """An abstract helper class to package specific service data using
     Bluetooth SIG defined 16-bit UUID flags to describe the data type."""
 
-    def __init__(self, uuid: int) -> None:
+    def __init__(self, uuid):
         self._type = struct.pack("<H", uuid)
         self._data = b""
 
     @property
-    def uuid(self) -> int:
+    def uuid(self):
         """This returns the 16-bit Service UUID as a `bytearray` in little
         endian. (read-only)"""
         return self._type
 
     @property
-    def data(self) -> bytearray:
+    def data(self):
         """The service's data. This is a `bytearray`, and its format is
         defined by relative Bluetooth Service Specifications (and GATT
         supplemental specifications)."""
         return self._data
 
     @data.setter
-    def data(self, value) -> None:
+    def data(self, value):
         self._data = value
 
     @property
-    def buffer(self) -> bytearray:
+    def buffer(self):
         """Get the representation of the instantiated object as an
         immutable `bytes` object (read-only)."""
         return bytes(self._type + self.data)
 
-    def __len__(self) -> int:
+    def __len__(self):
         """For convenience, this class is compatible with python's builtin
         :py:func:`len()` method. In this context, this will return the length
         of the object (in bytes) as it would appear in the advertisement
@@ -302,11 +296,11 @@ class TemperatureServiceData(ServiceData):
     """This derivitive of the `ServiceData` class can be used to represent
     temperature data values as a `float` value."""
 
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__(0x1809)
 
     @ServiceData.data.setter
-    def data(self, value: float) -> None:
+    def data(self, value):
         value = struct.pack("<i", int(value * 100) & 0xFFFFFF)
         self._data = value[:3] + bytes([0xFE])
 
@@ -315,11 +309,11 @@ class BatteryServiceData(ServiceData):
     """This derivitive of the `ServiceData` class can be used to represent
     battery charge percentage as a 1-byte value."""
 
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__(0x180F)
 
     @ServiceData.data.setter
-    def data(self, value: int) -> None:
+    def data(self, value):
         self._data = struct.pack(">B", value)
 
 
@@ -327,27 +321,27 @@ class UrlServiceData(ServiceData):
     """This derivitive of the `ServiceData` class can be used to represent
     URL data as a `bytes` value."""
 
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__(0xFEAA)
         self._type += bytes([0x10]) + struct.pack(">b", -25)
 
     @property
-    def pa_level_at_1_meter(self) -> int:
+    def pa_level_at_1_meter(self):
         """The TX power level (in dBm) at 1 meter from the nRF24L01. This
         defaults to -25 (due to testing when broadcasting with 0 dBm) and must
         be a 1-byte signed `int`."""
         return struct.unpack(">b", self._type[-1:])[0]
 
     @pa_level_at_1_meter.setter
-    def pa_level_at_1_meter(self, value: int) -> None:
+    def pa_level_at_1_meter(self, value):
         self._type = self._type[:-1] + struct.pack(">b", int(value))
 
     @property
-    def uuid(self) -> bytes:
+    def uuid(self):
         return self._type[:2]
 
     @ServiceData.data.setter
-    def data(self, value: str) -> None:
+    def data(self, value):
         value = value.replace("http://www.", "\x00")
         value = value.replace("https://www.", "\x01")
         value = value.replace("http://", "\x02")
